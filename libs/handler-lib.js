b06ce4febdb3d7e389dbe84d06fa0a6c6dbba02d
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-05-23 13:12:40 -0400

    regret hand coding the reconfiguration instead of copy pasting. Had to go back and copy and paste to ensure I caught all my little bugs to speed up the process. Forgot to test the reconfigured code last night. Everything is working now so that is exciting. List API is next.

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,21 +1,22 @@
 export default function handler(lambda) {
-    return function (event, context) {
-        return Promise.resolve()
-            .then(() => lambda(event, context))
-
-            .then((responseBody) => [200, responseBody])
-
-            .catch((e) => {
-                console.log(e);
-                return [500, { error: e.message }];
-            })
-            .then(([statusCode, body]) => ({
-                statusCode,
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Credentials": true,
-                },
-                body: JSON.stringify(body),
-            }));
-    };
+  return function (event, context) {
+    return Promise.resolve()
+      // Run the Lambda
+      .then(() => lambda(event, context))
+      // On success
+      .then((responseBody) => [200, responseBody])
+      // On failure
+      .catch((e) => {
+        return [500, { error: e.message }];
+      })
+      // Return HTTP response
+      .then(([statusCode, body]) => ({
+        statusCode,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(body),
+      }));
+  };
 }
